019114ee70332040052aaca70071cda1093814ea
VENKATESH KANCHANPALLY <38311122+venkatnetha@users.noreply.github.com>
2022-05-25 03:14:40 +0530

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,43 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+
+
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+
+// Replace this private key with your Ropsten account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.13",
+  networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [`0x${RINKEBY_PRIVATE_KEY}`],
+      gas: 2100000,
+      gasPrice: 8000000000,
+      saveDeployments: true,
+    }
+  }
+};
